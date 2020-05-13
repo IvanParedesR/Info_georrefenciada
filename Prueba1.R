a5010582d50884e63ddbef5d30e924be2c6462ce
Ivan Paredes <ivanresendiz25@gmail.com>
2020-05-13 16:52:14 -0500

    local change

--- Prueba1.R
+++ Prueba1.R
@@ -35,3 +35,55 @@ ggplot(calles) +
        subtitle = "Vías de circulación",
        caption = "fuente: OpenStreetMap",
        color = "velocidad máxima")
+
+library(tidyverse)
+library(osmdata)
+library(sf)
+library(ggmap)
+
+head(available_features())
+head(available_tags("amenity"))
+head(available_tags("shop"))
+
+q <- getbb("Madrid") %>%
+  opq() %>%
+  add_osm_feature("amenity", "cinema")
+
+str(q) #query structure
+
+
+cinema <- osmdata_sf(q)
+cinema
+
+mad_map <- get_map(getbb("Madrid"), maptype = "toner-background")
+
+#final map
+ggmap(mad_map)+
+  geom_sf(data = cinema$osm_points,
+          inherit.aes = FALSE,
+          colour = "#238443",
+          fill = "#004529",
+          alpha = .5,
+          size = 4,
+          shape = 21)+
+  labs(x = "", y = "")
+
+m <- c(-10, 30, 5, 46)
+
+#building the query
+q <- m %>% 
+  opq (timeout = 25*100) %>%
+  add_osm_feature("name", "Mercadona") %>%
+  add_osm_feature("shop", "supermarket")
+
+#query
+mercadona <- osmdata_sf(q)
+
+#final map
+ggplot(mercadona$osm_points)+
+  geom_sf(colour = "#08519c",
+          fill = "#08306b",
+          alpha = .5,
+          size = 1,
+          shape = 21)+
+  theme_void()
